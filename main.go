2bb7e85857ae92e6f60b94da0b628e452011e455
Eric Gourlaouen <gourlaoueneric@gmail.com>
2016-11-29 01:52:34 +0900

    a bit of cleaning: commenting + line breaks

--- main.go
+++ main.go
@@ -11,13 +11,14 @@ import (
 )
 
 func main() {
+	// Reading the configuration file
 	conf, err := confFileReader()
 	if err != nil {
-		fmt.Println("Couldn't get configuration information: " + err.Error())
+		fmt.Println("Couldn't get configuration information: " + err.Error() + " // Please make sure that there is a configuration file conf.json in the current directory that respects the format of the example.")
 		os.Exit(0)
 	}
 
-	fmt.Println("Connecting...")
+	// Connection to the broker
 	client := TTNMQTT.NewClient(nil, "ttn-opensensors", conf.TTN.ApplicationID, conf.TTN.AccessKey, "tcp://"+conf.TTN.Region+".thethings.network:1883")
 	if err := client.Connect(); err != nil {
 		fmt.Println("Couldn't connect: " + err.Error())
@@ -25,13 +26,16 @@ func main() {
 	}
 	fmt.Println("Connection complete.")
 
+	// Initializing OpenSensors Client
 	var OSCli OpenSensorsClient
 	OSCli.conf = conf.OpenSensors
-	fmt.Println("Client initialized.")
 
-	token := client.SubscribeDeviceUplink(conf.TTN.ApplicationID, conf.TTN.DeviceID, func(client TTNMQTT.Client, appID string, devID string, req types.UplinkMessage) {
-		fmt.Println("\n\n=========================\nMESSAGE RECEPTION\n")
+	// Saving message handling function
+	messageHandler := func(client TTNMQTT.Client, appID string, devID string, req types.UplinkMessage) {
+		fmt.Print("\n\n=========================\nMESSAGE RECEPTION\n\n")
+
 		if rep, err := OSCli.postMQTTPayload(req.PayloadFields); err != nil || ErrorResponse(rep) {
+			// Error handling
 			if ErrorResponse(rep) {
 				fmt.Println("Error " + strconv.Itoa(rep.StatusCode) + ": " + ReaderToString(rep.Body))
 			} else if err != nil {
@@ -42,14 +46,17 @@ func main() {
 		} else {
 			fmt.Println("Message successfuly retransmitted to OpenSensors.")
 		}
-	})
+	}
 
+	// Subscribing
+	token := client.SubscribeDeviceUplink(conf.TTN.ApplicationID, conf.TTN.DeviceID, messageHandler)
 	fmt.Println("Subscription complete - waiting for messages...")
 	token.Wait()
 	if err := token.Error(); err != nil {
-		fmt.Println("Could not subscribe")
+		fmt.Println("Could not subscribe: " + err.Error())
 		os.Exit(0)
 	}
 
+	// Launching program main process as idle mode
 	select {}
 }
--- opensensors.go
+++ opensensors.go
@@ -9,12 +9,14 @@ type OpenSensorsClient struct {
 	conf OpenSensorsConf
 }
 
+// getAPICallURL returns the URI to post the data to
 func (cli OpenSensorsClient) getAPICallURL() string {
 	return cli.conf.APIURL + "topics//users/" + cli.conf.Username + "/" +
 		cli.conf.TopicName + "?client-id=" + cli.conf.DeviceID + "&password=" +
 		cli.conf.DevicePassword
 }
 
+// postToAPI takes a data payload, whatever its format, and posts it to the OpenSensorsAPI
 func (cli OpenSensorsClient) postToAPI(data []byte) (*http.Response, error) {
 	httpClient := &http.Client{}
 	bytesData := bytes.NewReader(data)
@@ -32,6 +34,7 @@ type OSPayload struct {
 	data map[string]interface{}
 }
 
+// encapsulateIntoData takes a JSON unmarshalled object, and wraps it as a string in another JSON unmarshalled object
 func encapsulateIntoData(data map[string]interface{}) (map[string]interface{}, error) {
 	stringifiedData, err := json.Marshal(data)
 	if err != nil {
@@ -42,6 +45,7 @@ func encapsulateIntoData(data map[string]interface{}) (map[string]interface{}, e
 	return payload, nil
 }
 
+// postMQTTPayload takes a JSON unmarshalled object containing the data to post, and posts it to OpenSensors
 func (cli OpenSensorsClient) postMQTTPayload(data map[string]interface{}) (*http.Response, error) {
 	encapsulatedData, err := encapsulateIntoData(data)
 	if err != nil {
